f3c1d554b46c5f18f19b80c920060afad351535b
plee99 <peter_lee_16@sfu.ca>
2023-06-12 15:18:46 -0700

    push

---
+++ src/main/java/com/cmpt276/assn2/Assn2Application.java
@@ -0,0 +1,13 @@
+package com.cmpt276.assn2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assn2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assn2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmpt276/assn2/Assn2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpt276.assn2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assn2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
